8eb5b65014e88a47d7ff8de18d9f9b29a4641802
Hen3 <hendrixperia@gmaill.com>
2024-06-02 18:50:05 +0200

    Removed django

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 # Plex API
-import os
 import threading
 
 from workers.main_worker import MainWorker
@@ -20,21 +19,6 @@ def main():
     except KeyboardInterrupt:
         pass
     worker.stop()
-
-
-def start_django():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webui.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(["main.py", "runserver"])
-
 
 if __name__ == "__main__":
     main()
